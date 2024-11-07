b58cc06d51767ee2e1de57c81a0f31b003bac783
Christopher Hamilton <chrish001@live.com.au>
2024-11-07 20:54:39 +1030

    adds test data handling

--- Assignment2/load_data.py
+++ Assignment2/load_data.py
@@ -46,26 +46,12 @@ def get_validation_data():
     return {"data": data, "labels": labels}
 
 
-def get_test_data(image_width, image_height):
+def get_test_data():
     path = "/home/chris/uni/cifar-10-batches-py/test_batch"
 
     batch_data = get_data(path)
     data = batch_data["data"]
     labels = batch_data["labels"]
-
-    for i in range(len(data)):
-        img = data[i]
-        red = img[:1024].reshape((32, 32))
-        green = img[1024:2048].reshape((32, 32))
-        blue = img[2048:].reshape((32, 32))
-        img_rgb = np.stack((red, green, blue), axis=-1)
-
-        # Resize and preprocess for VGG19
-        img_resized = tf.image.resize(img_rgb, (image_width, image_height)).numpy()
-        img_preprocessed = tf.keras.applications.vgg19.preprocess_input(img_resized)
-
-        data[i] = img_preprocessed
-        labels[i] = tf.keras.utils.to_categorical(labels[i], num_classes)
 
     # data -- a 10000x3072 numpy array of uint8s. Each row of the array stores a 32x32 colour image. The first 1024 entries contain the red channel values, the next 1024 the green, and the final 1024 the blue. The image is stored in row-major order, so that the first 32 entries of the array are the red channel values of the first row of the image.
     # labels -- a list of 10000 numbers in the range 0-9. The number at index i indicates the label of the ith image in the array data.
@@ -119,3 +105,32 @@ def data_generator(data, labels, image_width, image_height, batch_size=8):
                 )
 
             yield np.array(batch_images), np.array(batch_labels)
+
+
+def test_data_generator(data, labels, image_width, image_height, batch_size=8):
+    num_samples = len(data)
+    indices = np.arange(num_samples)  # No need to shuffle for evaluation
+
+    for start in range(0, num_samples, batch_size):
+        end = min(start + batch_size, num_samples)
+        batch_indices = indices[start:end]
+
+        batch_images = []
+        batch_labels = []
+
+        for i in batch_indices:
+            img = data[i]
+            red = img[:1024].reshape((32, 32))
+            green = img[1024:2048].reshape((32, 32))
+            blue = img[2048:].reshape((32, 32))
+            img_rgb = np.stack((red, green, blue), axis=-1)
+
+            # Resize and preprocess
+            img_resized = tf.image.resize(img_rgb, (image_width, image_height)).numpy()
+            img_preprocessed = tf.keras.applications.vgg19.preprocess_input(img_resized)
+
+            batch_images.append(img_preprocessed)
+
+            batch_labels.append(tf.keras.utils.to_categorical(labels[i], num_classes))
+
+        yield np.array(batch_images), np.array(batch_labels)
