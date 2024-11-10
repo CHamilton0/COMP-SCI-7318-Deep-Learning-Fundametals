85a1896e722d5d91098cf2a13bef9e7bfbbc330a
Christopher Hamilton <chrish001@live.com.au>
2024-11-10 16:38:19 +1030

    removes unused files

--- Assignment2/resnet18.py
+++ Assignment2/resnet18.py
@@ -4,41 +4,6 @@ import keras
 from keras import layers
 
 
-class ConvolutionBlock(tf.keras.Model):
-    """
-    This class will create a convolution block for the resnet model
-    by subclassing Model class
-    """
-
-    def __init__(self, filters, kernel_size):
-        super(ConvolutionBlock, self).__init__(name="")
-
-        self.conv1 = layers.Conv2D(filters, kernel_size, padding="same")
-        self.bn1 = layers.BatchNormalization()
-
-        self.conv2 = layers.Conv2D(filters, kernel_size, padding="same")
-        self.bn2 = layers.BatchNormalization()
-
-        self.act = layers.Activation("relu")
-        self.add = layers.Add()
-
-    def call(self, input_tensor):
-        # Block 1: Conv--> BN--> ReLU
-        x = self.conv1(input_tensor)
-        x = self.bn1(x)
-        x = self.act(x)
-
-        # Block 2: Conv--> BN--> ReLU
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.act(x)
-
-        # skip connection
-        x = self.add([x, input_tensor])
-        x = self.act(x)
-        return x
-
-
 class IdentityBlock(tf.keras.Model):
     """
     This class will create an identity block for the resnet model
@@ -81,16 +46,11 @@ class ResNet18(tf.keras.Model):
         self.bn = layers.BatchNormalization()
         self.act = layers.Activation("relu")
         self.max_pool = layers.MaxPool2D((3, 3))
+        self.drop_out = layers.Dropout(0.3)
 
         # create Identity blocks
-        self.conv1 = ConvolutionBlock(64, 3)
         self.id1 = IdentityBlock(64, 3)
-        self.conv2 = ConvolutionBlock(128, 3)
-        self.id2 = IdentityBlock(128, 3)
-        self.conv3 = ConvolutionBlock(256, 3)
-        self.id3 = IdentityBlock(256, 3)
-        self.conv4 = ConvolutionBlock(512, 3)
-        self.id4 = IdentityBlock(512, 3)
+        self.id2 = IdentityBlock(64, 3)
 
         self.global_pool = layers.GlobalAveragePooling2D()
         self.classifier = layers.Dense(num_classes, activation="softmax")
@@ -100,19 +60,13 @@ class ResNet18(tf.keras.Model):
         x = self.bn(x)
         x = self.act(x)
         x = self.max_pool(x)
+        x = self.drop_out(x)
 
         # call identity blocks
-        x = self.conv1(x)
         x = self.id1(x)
-        x = layers.Conv2D(128, 3, strides=2, padding='same')(x)
-        x = self.conv2(x)
+        x = self.drop_out(x)
         x = self.id2(x)
-        x = layers.Conv2D(256, 3, strides=2, padding='same')(x)
-        x = self.conv3(x)
-        x = self.id3(x)
-        x = layers.Conv2D(512, 3, strides=2, padding='same')(x)
-        x = self.conv4(x)
-        x = self.id4(x)
+        x = self.drop_out(x)
 
         x = self.global_pool(x)
         return self.classifier(x)
